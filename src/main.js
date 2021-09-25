05e2b54a6ff54aa2a0891e8add11599a18e95662
ymak@cadabra.studio <denysgdmgroup@gmail.com>
2021-09-25 23:36:44 +0300

    lesson5 is ready

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import DateFilter from './filters/date.filter'
+import Vuelidate from 'vuelidate'
 
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize'
 
 Vue.config.productionTip = false
 Vue.filter('filterDate', DateFilter)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
